3dc6309192370eefb848a33328960f6205253ef5
zbeboy <863052317@qq.com>
2019-09-30 00:17:38 +0800

    定时

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,31 @@
+import 'dart:async';
+
 import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
+  directives: [coreDirectives, formDirectives],
 )
-class AppComponent {
-  var name = 'Angular';
+class AppComponent implements AfterViewInit {
+  var startTime = '2019-10-13 09:10:00';
+  var endTime = '2019-10-13 09:40:00';
+  var residueTime;
+  var realName;
+  var studentNumber;
+
+  void calculationResidueTime() {
+    Timer.periodic(Duration(minutes: 1), handleResidueTime);
+  }
+
+  void handleResidueTime(Timer timer) {
+    residueTime =
+        DateTime(2019, 9, 30, 1, 0, 0, 0).difference(DateTime.now()).inMinutes;
+  }
+
+  @override
+  void ngAfterViewInit() {
+    calculationResidueTime();
+  }
 }
