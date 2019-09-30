28b2ac2d7fab552be9a8bf04d170aee7f51fcab3
zbeboy <863052317@qq.com>
2019-09-30 11:50:50 +0800

    增加选题

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
@@ -8,24 +9,39 @@ import 'package:angular_forms/angular_forms.dart';
   templateUrl: 'app_component.html',
   directives: [coreDirectives, formDirectives],
 )
-class AppComponent implements AfterViewInit {
-  var startTime = '2019-10-13 09:10:00';
-  var endTime = '2019-10-13 09:40:00';
+class AppComponent {
+  var startTime = DateTime(2019, 9, 30, 9, 10, 0, 0);
+  var endTime = DateTime(2019, 9, 30, 18, 40, 0, 0);
   var residueTime;
   var realName;
   var studentNumber;
+  var subject;
+  List<int> subjectNum;
 
-  void calculationResidueTime() {
-    Timer.periodic(Duration(minutes: 1), handleResidueTime);
+  AppComponent() {
+    var nowDate = DateTime.now();
+    // 在考试范围
+    if (nowDate.isAfter(startTime) && nowDate.isBefore(endTime)) {
+      residueTime = endTime.difference(nowDate).inMinutes;
+      Timer.periodic(Duration(minutes: 1), handleResidueTime);
+      var i = 0;
+      var r = Random();
+      subjectNum = [];
+      while(i<20){
+        var num = r.nextInt(100);
+        if (!subjectNum.contains(num)) {
+          subjectNum.add(num);
+          i++;
+        }
+      }
+      subject = '以下答案正确的是：';
+    } else {
+      residueTime = endTime.difference(startTime).inMinutes;
+      subject = '未在考试时间范围';
+    }
   }
 
   void handleResidueTime(Timer timer) {
-    residueTime =
-        DateTime(2019, 9, 30, 1, 0, 0, 0).difference(DateTime.now()).inMinutes;
-  }
-
-  @override
-  void ngAfterViewInit() {
-    calculationResidueTime();
+    residueTime = endTime.difference(DateTime.now()).inMinutes;
   }
 }
